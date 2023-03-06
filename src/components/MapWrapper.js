8ba4120671eeef4133119fbce114c87f019a104b
SquirrelloDev <olejnike2000@gmail.com>
2023-03-06 15:14:31 +0100

    Added custom icon

--- src/components/MapWrapper.js
+++ src/components/MapWrapper.js
@@ -1,19 +1,17 @@
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import classes from "../sass/Map.module.scss";
+import {iconMarker} from "./UI/MarkerIcon";
 
 const MapWrapper = () => {
+
   return(
       <section className={classes.map}>
-      {/*TODO: add a leaflet map*/}
           <MapContainer className={classes.map__container} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
               <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[51.505, -0.09]}>
-                  <Popup>
-                      A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
+              <Marker position={[51.505, -0.09]} icon={iconMarker}>
               </Marker>
           </MapContainer>
       </section>
---
+++ src/components/UI/MarkerIcon.js
@@ -0,0 +1,9 @@
+import L from 'leaflet';
+import iconimg from '../../img/icon-location.svg'
+const iconMarker = new L.Icon({
+    iconUrl: iconimg,
+    iconSize:     [46, 56], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+});
+
+export { iconMarker };
