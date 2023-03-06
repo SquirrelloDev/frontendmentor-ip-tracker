5e73e47fbc5bd3138b593ad31c566d0b7376e27f
SquirrelloDev <olejnike2000@gmail.com>
2023-03-06 14:31:36 +0100

    Added styles for ip box

--- src/components/IPBox.js
+++ src/components/IPBox.js
@@ -1,12 +1,13 @@
 import IPBoxItem from "./UI/IPBoxItem";
+import classes from "../sass/Menu.module.scss";
 
 const IPBox = () => {
   return (
-      <div>
-        <IPBoxItem/>
-        <IPBoxItem/>
-        <IPBoxItem/>
-        <IPBoxItem/>
+      <div className={classes['menu__ip-box']}>
+        <IPBoxItem title={'Ip address'} value={'192.168.174.101'}/>
+        <IPBoxItem title={'Location'} value={'London'}/>
+        <IPBoxItem title={'timezone'} value={'UTC -5:00'}/>
+        <IPBoxItem title={'isp'} value={'SpaceX Starink'}/>
       </div>
   )
 }
--- src/components/UI/IPBoxItem.js
+++ src/components/UI/IPBoxItem.js
@@ -1,6 +1,8 @@
+import classes from "../../sass/Menu.module.scss";
+
 const IPBoxItem = ({title, value}) => {
   return(
-      <div>
+      <div className={classes['menu__ip-box__item']}>
           <h2>{title}</h2>
           <h3>{value}</h3>
           {/*TODO: add vertical line for desktop devices*/}
