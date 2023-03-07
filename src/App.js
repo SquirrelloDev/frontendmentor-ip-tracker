e37cf2c7fa1b49607e242e02324cf871e6cd8eaf
SquirrelloDev <olejnike2000@gmail.com>
2023-03-07 22:38:33 +0100

    Removed geolocation custom hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import Menu from "./components/Menu";
 import MapWrapper from "./components/MapWrapper";
 import {useCallback, useEffect, useState} from "react";
 import useHttp from "./hooks/use-http";
-import useGeolocation from "./hooks/use-geolocation";
 
 function App() {
     const [ipInfo, setIpInfo] = useState({});
     const [location, setLocation] = useState([]);
     const {getUserIP,getIP} = useHttp()
-    const {geoAvailable,userPosition,getUserLocation} = useGeolocation();
+
     const setIp = useCallback( (info) => {
       setIpInfo(info);
       setLocation(info.location);
